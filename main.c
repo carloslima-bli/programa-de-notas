f7f947e9789267991bf227898c7ed71b0a49b478
Thomas <thomas.cifuentes@udla.edu.ec>
2026-05-19 11:46:54 -0500

    declaracion de matriz e ingreso de datos estudiantes

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define MAX_ESTUDIANTES 100
+#define ASIGNATURAS 3
+ 
+int main(){
+
+
+    float calificaciones[MAX_ESTUDIANTES][ASIGNATURAS];
+    int estudiantes;
+ 
+    printf("Ingrese el numero de estudiantes (Maximo %d): ", MAX_ESTUDIANTES);
+    scanf("%d", &estudiantes);
+ 
+
+    if (estudiantes <= 0 || estudiantes > MAX_ESTUDIANTES) {
+        printf("Numero de estudiantes no valido.\n");
+ }
+        return 0;
+        }
